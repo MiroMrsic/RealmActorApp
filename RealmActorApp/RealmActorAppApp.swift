4fce08415f9cd0862588c8b8ba8a2b73468b8d1f
Miroslav <miroslav@resser-solutions.com>
2025-07-01 10:01:39 +0200

    Initial Commit

---
+++ RealmActorApp/RealmActorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmActorAppApp.swift
+//  RealmActorApp
+//
+//  Created by Miroslav Mrsic on 01.07.2025..
+//
+
+import SwiftUI
+
+@main
+struct RealmActorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
